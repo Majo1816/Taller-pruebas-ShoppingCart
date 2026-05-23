cd80d07862765401dc5732ac13b9fdf21c9f9fee
majo <mariajoseaguilar188@gmail.com>
2026-05-22 23:14:19 -0500

    fix: corregir calculo en applyDiscount

--- shopping-cart/src/main/java/ShoppingCart.java
+++ shopping-cart/src/main/java/ShoppingCart.java
@@ -29,7 +29,7 @@ public class ShoppingCart {
      if (percentage < 0 || percentage > 100) {
         throw new IllegalArgumentException("Invalid discount percentage");
      }
-        return 0; 
+        return getTotal() *(1-porcentaje/100.0); 
 }
 
     public int getItemCount() {
